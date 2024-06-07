e1925bdd1624dbaed17b1227b70c7288f362f8c6
Aniket Bhalla <aniketbhalla2544@gmail.com>
2024-06-07 17:33:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/api/products', (req, res) => {
         category: 'Fitness',
       },
       {
-        id: 7,
+        id: 78,
         name: 'Noushad product',
         description:
           'Non-slip, environmentally friendly yoga mat made from natural rubber. Ideal for both beginners and advanced yoga practitioners.',
